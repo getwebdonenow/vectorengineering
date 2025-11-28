4765e813313bf38e90233757d9b85ae504ba999b
Abhijith-k-rajendran <81981595+Abhijith-k-rajendran@users.noreply.github.com>
2025-11-28 23:59:50 +0530

    ""

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-"use client";
-import React from 'react';
-import Navbar from '@/components/ndt/Navbar';
-import HeroSection from '@/components/ndt/HeroSection';
-import ServicesSection from '@/components/ndt/ServicesSection';
-import AboutSection from '@/components/ndt/AboutSection';
-import IndustriesSection from '@/components/ndt/IndustriesSection';
-import WhyChooseUs from '@/components/ndt/WhyChooseUs';
-import ContactSection from '@/components/ndt/ContactSection';
-import Footer from '@/components/ndt/Footer';
+import React from "react";
+import Navbar from "@/components/ndt/Navbar";
+import HeroSection from "@/components/ndt/HeroSection";
+import ServicesSection from "@/components/ndt/ServicesSection";
+import AboutSection from "@/components/ndt/AboutSection";
+import IndustriesSection from "@/components/ndt/IndustriesSection";
+import WhyChooseUs from "@/components/ndt/WhyChooseUs";
+import ContactSection from "@/components/ndt/ContactSection";
+import Footer from "@/components/ndt/Footer";
 
 export default function Home() {
   return (
