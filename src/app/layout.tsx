c9ff5445b0eefdf799d567ff687d07f7b09bb557
Abhijith-k-rajendran <81981595+Abhijith-k-rajendran@users.noreply.github.com>
2025-12-04 20:56:04 +0530

    ""

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import FloatingWhatsApp from "@/components/FloatingWhatsapp/FloatingWhatsapp";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
+        <Analytics />
         <FloatingWhatsApp
           number="971544942878"
           message="Hello! I would like to inquire about your services."
